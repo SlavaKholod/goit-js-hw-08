95be865c16ef65b3baedfdd01c3b58d3f3b12893
SlavaKholod <kholod.v.job@gmail.com>
2023-03-12 12:40:15 +0200

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = {
+    container: document.querySelector(".gallery"),
+  };
+  
+  function addMarckup() {
+    const listItems = galleryItems
+      .map(
+        (element) =>
+          `<li><a class="gallery__link" href=${element.original}>
+            <img
+              class="gallery__image"
+              src=${element.preview}
+              data-source=${element.original}
+              alt=${element.description}
+              width=340
+            />
+          </a></li>`
+      )
+      .join("");
+  
+    gallery.container.insertAdjacentHTML("afterbegin", listItems);
+  }
+  addMarckup();
+  
+  var lightbox = new SimpleLightbox(".gallery a", {
+    captionSelector: "img",
+    captionType: "attr",
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  
